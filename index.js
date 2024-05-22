e83dd945a3f0fb02832297626a180a0730260a5a
Tien-Nhan <tiennhan98ars@gmail.com>
2024-05-22 22:33:38 +0700

    nop-bo-sung-bai-tap

--- index.js
+++ index.js
@@ -36,12 +36,12 @@ document.getElementById("ketqua1-for").innerHTML = timNhoNhatFor();
 //* Yêu cầu câu số 2
 function sumWhile(x, n) {
     var sum = 0;
-    var conSo = 1;
+    var i = 1;
     var numX = x;
-    while (conSo <= n) {
+    while (i <= n) {
         sum = sum + numX;
         numX = numX * x;
-        conSo = conSo + 1;
+        i = i + 1;
     }
     return sum;
 }
@@ -49,20 +49,20 @@ function sumWhile(x, n) {
 
 function sumDoWhile(x, n) {
     var sum = 0;
-    var conSo = 1;
+    var i = 1;
     var numX = x;
     do {
         sum = sum + numX;
         numX = numX * x;
-        conSo = conSo + 1;
-    } while (conSo <= n);
+        i = i + 1;
+    } while (i <= n);
     return sum;
 }
 
 function sumFor(x, n) {
     var sum = 0;
     var numX = x;
-    for (var conSo = 1; conSo <= n; conSo = conSo + 1) {
+    for (var i = 1; i <= n; i = i + 1) {
         sum = sum + numX;
         numX = numX * x;
     }
@@ -89,28 +89,28 @@ function handleSum() {
 //* Yêu cầu câu số 3
 function giaiThuaWhile(n) {
     var giaiThua = 1;
-    var conSo = 1;
-    while (conSo <= n) {
-        giaiThua = giaiThua * conSo;
-        conSo = conSo + 1;
+    var i = 1;
+    while (i <= n) {
+        giaiThua = giaiThua * i;
+        i = i + 1;
     }
     return giaiThua;
 }
 
 function giaiThuaDoWhile(n) {
     var giaiThua = 1;
-    var conSo = 1;
+    var i = 1;
     do {
-        giaiThua = giaiThua * conSo;
-        conSo = conSo + 1;
-    } while (conSo <= n);
+        giaiThua = giaiThua * i;
+        i = i + 1;
+    } while (i <= n);
     return giaiThua;
 }
 
 function giaiThuaFor(n) {
     var giaiThua = 1;
-    for (var conSo = 1; conSo <= n; conSo = conSo + 1) {
-        giaiThua = giaiThua * conSo;
+    for (var i = 1; i <= n; i = i + 1) {
+        giaiThua = giaiThua * i;
     }
     return giaiThua;
 }
@@ -136,16 +136,16 @@ function handleGiaiThua() {
 function taoDivWhile() {
     var container = document.getElementById('divContainer');
     var content = "";
-    var conSo = 1;
-    while (conSo <= 10) {
+    var i = 1;
+    while (i <= 10) {
         var color;
-        if (conSo % 2 === 0) {
+        if (i % 2 === 0) {
             color = 'background-color: red;';
         } else {
             color = 'background-color: blue;';
         }
-        content += '<div style="' + color + '">Div ' + conSo + '</div>';
-        conSo = conSo + 1;
+        content += '<div style="' + color + '">Div ' + i + '</div>';
+        i = i + 1;
     }
     container.innerHTML = content;
 }
@@ -153,31 +153,82 @@ function taoDivWhile() {
 function taoDivDoWhile() {
     var container = document.getElementById('divContainer');
     var content = "";
-    var conSo = 1;
+    var i = 1;
     do {
         var color;
-        if (conSo % 2 === 0) {
+        if (i % 2 === 0) {
             color = 'background-color: red;';
         } else {
             color = 'background-color: blue;';
         }
-        content += '<div style="' + color + '">Div ' + conSo + '</div>';
-        conSo = conSo + 1;
-    } while (conSo <= 10);
+        content += '<div style="' + color + '">Div ' + i + '</div>';
+        i = i + 1;
+    } while (i <= 10);
     container.innerHTML = content;
 }
 
 function taoDivFor() {
     var container = document.getElementById('divContainer');
     var content = "";
-    for (var conSo = 1; conSo <= 10; conSo = conSo + 1) {
+    for (var i = 1; i <= 10; i = i + 1) {
         var color;
-        if (conSo % 2 === 0) {
+        if (i % 2 === 0) {
             color = 'background-color: red;';
         } else {
             color = 'background-color: blue;';
         }
-        content += '<div style="' + color + '">Div ' + conSo + '</div>';
+        content += '<div style="' + color + '">Div ' + i + '</div>';
     }
     container.innerHTML = content;
+}
+
+
+
+//* Yêu cầu câu số 5
+function soNguyenTo(num) {
+    if (num <= 1) return false;
+    for (var i = 2; i < num; i = i + 1) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function inNguyenToWhile() {
+    var output = document.getElementById('nguyenToOutput');
+    var input = document.getElementById('nguyenToInput').value;
+    var cacNguyenTo = '';
+    var i = 1;
+    while (i <= input) {
+        if (soNguyenTo(i)) {
+            cacNguyenTo += '<span>' + i + '</span>';
+        }
+        i = i + 1;
+    }
+    output.innerHTML = cacNguyenTo;
+}
+
+function inNguyenToDoWhile() {
+    var output = document.getElementById('nguyenToOutput');
+    var input = document.getElementById('nguyenToInput').value;
+    var cacNguyenTo = '';
+    var i = 1;
+    do {
+        if (soNguyenTo(i)) {
+            cacNguyenTo += '<span>' + i + '</span>';
+        }
+        i = i + 1;
+    } while (i <= input);
+    output.innerHTML = cacNguyenTo;
+}
+
+function inNguyenToFor() {
+    var output = document.getElementById('nguyenToOutput');
+    var input = document.getElementById('nguyenToInput').value;
+    var cacNguyenTo = '';
+    for (var i = 1; i <= input; i = i + 1) {
+        if (soNguyenTo(i)) {
+            cacNguyenTo += '<span>' + i + '</span>';
+        }
+    }
+    output.innerHTML = cacNguyenTo;
 }
